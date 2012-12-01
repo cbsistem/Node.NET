f7e7fdb3964b1658dd10ae578ddd9519503e6803
erin100280 <erinbradford100280@gmail.com>
2012-12-01 15:50:33 -0800

    scnd

---
+++ core/main.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Erin
+ * Date: 12/1/2012
+ * Time: 3:40 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace core
+{
+	/// <summary>
+	/// Description of main.
+	/// </summary>
+	public class main
+	{
+		public main()
+		{
+		}
+	}
+}
